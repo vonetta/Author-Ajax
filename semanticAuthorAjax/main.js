2bdc39ec74a06071e7be34e6572ee33cbce9daf1
Vonetta <vonettastevenson@gmail.com>
2017-12-04 10:31:53 -0800

    beginning of delete and update

--- semanticAuthorAjax/main.js
+++ semanticAuthorAjax/main.js
@@ -1,12 +1,16 @@
 ﻿$(document).ready(function () {
     $(".loadAuthors").on("click", loadAuthors);
     $(".btnSubmit").on("click", submitNewAuthor);
+    $(".author-list").on("click", ".deleteBtn", deleteAuthor);
+    $(".author-list").on("click", ".author", updateAuthor);
 });
 
 var url = "http://sabioapi2.azurewebsites.net/api/authors";
 var header = { "SABIO-AUTH": "U7E2M0SUX" };
+activeId = null;
 
 function success(data, status, xhr) {
+    console.log(data)
     $(".author-list").empty();
     for (var i = 0; i < data.items.length; i++) {
         addAuthor(data.items[i]);
@@ -15,6 +19,10 @@ function success(data, status, xhr) {
 
 function successChange(data, status, xhr) {
     loadAuthors(success)
+}
+
+function successGet(data, status, xhr) {
+    updateForm(data.item);
 }
 
 function error(xhr, status, errorText) {
@@ -34,13 +42,35 @@ function loadAuthors() {
 
 function submitNewAuthor() {
     var data = readForm();
-    console.log(data);
+    if (activeId) {
+        data.id = activeId;
+        updateAuthorAjax(data, successChange);
+        activeId = null;
+    }
+    else {
     addAuthorAjax(data, successChange, error);
+    }
+}
+
+function deleteAuthor(e) {
+    e.stopPropagation()
+    console.log("deleting");
+    var author = $(this).closest("div.author");
+    console.log(author)
+    var id = author.data("authorId");
+    console.log(id)
+   deleteAuthorAjax(id, successChange);
+}
+
+function updateAuthor() {
+    var id = $(this).data("authorId");
+    getAuthorIdAjax(id, successGet)
 }
 
 function loadTemplate() {
     return $($("#template").html());
 }
+
 function addAuthor(data) {
     var template = loadTemplate();
 
@@ -60,6 +90,15 @@ function readForm() {
     return formData;
 }
 
+function updateForm(data) {
+    $("#fName").val(data.firtName);
+    $("#lName").val(data.lastName);
+    $("#age").val(data.age);
+    $("#salary").val(data.salary);
+
+    activeId = data.id
+}
+
 function addAuthorAjax(data, success, error) {
     var settings = {
         type: "POST",
@@ -71,3 +110,36 @@ function addAuthorAjax(data, success, error) {
     }
     $.ajax(url, settings)
 }
+function getAuthorIdAjax(id, success, error) {
+    var settings = {
+        headers: header,
+        type: "GET",
+        success: success,
+        error: error
+    }
+    $.ajax(url + "/" + id + settings);
+}
+function deleteAuthorAjax(id, success, error) {
+    var settings = {
+        headers: header,
+        type: "DELETE",
+        success: success,
+        error, error
+    }
+    $.ajax(url + "/" + id, settings);
+}
+function updateAuthorAjax(data, success, error) {
+    var settings = {
+        headers: header,
+        type: "PUT",
+        success: success,
+        error: error,
+        contentType: "application/json",
+        data: JSON.stringify(data)
+    }
+    $.ajax(url + "/" + data.id, settings);
+}
+
+
+
+
